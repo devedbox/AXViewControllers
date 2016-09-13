6f451c1bd64c4a012f65639dee4e822c3398fdc3
devedbox <862099730@qq.com>
2016-09-13 10:19:16 +0800

    Initial Commit

---
+++ AXViewControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXViewControllers
+//
+//  Created by devedbox on 2016/9/13.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AXViewControllers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AXViewControllers
+//
+//  Created by devedbox on 2016/9/13.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
